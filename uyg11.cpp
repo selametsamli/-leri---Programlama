3a80a5ddb0b76cf0c32e9fd450b9dda602e6b91b
Selamet Şamlı <selamet96@gmail.com>
2018-05-15 13:25:58 +0300

    yapici

---
+++ uyg11.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std; 
+
+
+class Dikdortgen {
+
+    int en,boy;
+public:
+    Dikdortgen(){
+        en = 5;
+        boy = 3;
+    }
+    Dikdortgen(int gEn, int gBoy){
+        en = gEn;
+        boy = gBoy;
+    }
+
+    int getEn(){
+        return en;
+    }
+    int getBoy(){
+        return boy;
+    }
+
+};
+
+
+
+int main (){
+
+    Dikdortgen d1;
+    Dikdortgen d2(5,12);
+
+    cout<<d1.getEn()<<" "<<d1.getBoy()<<endl;
+    cout<<d2.getEn()<<" "<<d2.getBoy()<<endl;
+
+    return 0;
+}
